a863e6eb2e4d82fee7850b778d9a68ebe7842a9f
HideyaMasutani <h.masutani0619@gmail.com>
2020-12-27 08:26:36 +0000

    commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  root to: 'homes#home'
+
+  resources :books
+
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
